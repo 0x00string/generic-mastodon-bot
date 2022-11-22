3c98ea418e73859dcfd13cae7340a3878c36f08c
xor eax,eax <0x00string@gmail.com>
2022-11-22 03:26:21 -0600

    Update generic-mastodon-bot.py
    blah blah blah whatever

--- generic-mastodon-bot.py
+++ generic-mastodon-bot.py
@@ -1,8 +1,9 @@
-import argparse, urllib.request, sqlite3, datetime, pytz, time, youtube_dl, os.path, json, mastodon
+import argparse, urllib.request, sqlite3, datetime, pytz, time, youtube_dl, os.path, json, mastodon, random, re
 from mastodon import Mastodon
+from html.parser import HTMLParser
 
-LOGLEVEL = 5
 DB_PATH = "./prefs.sqlite3"
+YTDL_VERBOSE = False
 DOWNLOAD_METHOD = "direct"
 DOWNLOAD_DIRECTORY = "./downloaded/"
 DOWNLOAD_REMOTE_URLS = True
@@ -15,31 +16,69 @@ def parseArguments():
     parser.add_argument('-u','--url', help='mastodon server url', required=True)
     return vars(parser.parse_args())
 
-# utility functions
-
-def log(text, category="info"):# all > debug > info > error > none
-    if (LOGLEVEL < 1):
-        return
-    date = str(datetime.datetime.now(pytz.timezone('US/Central')))
-    if (("info" in category) and (LOGLEVEL > 1)):
-        print("[INFO " + date + "]:" + str(text))
-    elif(("debug" in category) and (LOGLEVEL > 2)):
-        print("[DEBUG " + date + "]:" + str(text))
-    elif(("error" in category) and (LOGLEVEL > 0)):
-        print("[ERROR " + date + "]:" + str(text))
-    else:
-        print("[MISC " + date + "]:" + str(text))
+def log(message, category='info'):
+    print('[%s][%s]: %s' % (category, str(datetime.datetime.now(pytz.timezone('US/Central')).strftime("%Y-%m-%d %H:%M:%S")), message))
 
 def getFreeSpace():
     statvfs = os.statvfs('/')
     return ((statvfs.f_frsize * statvfs.f_bavail) / 1073741824) # in gigabytes
 
-# auth, etc
+class HTMLFilter(HTMLParser):
+    text = ""
+    def handle_data(self, data):
+        self.text += data
+
+def stripHTML(html):
+    f = HTMLFilter()
+    f.feed(html)
+    return f.text.encode('utf-16','surrogatepass').decode('utf-16')
+
+def hideHashtags(text):# [](#hidehashtags), content_type=text/markdown
+    return re.sub(r'(#[a-zA-Z0-9_]+)', r'\[\]\(\\1\)', text)
+
+def spongeCase(text):# LiKe tHiS
+    d = 0
+    o = ""
+    for c in text:
+        if d == 0:
+            d = 1
+            o += c.upper()
+        else:
+            d = 0
+            o += c.lower()
+    return o
+
+def erraticFormatting(text):# randomly switch between markdown formattings
+    o = ""
+    m = {
+        '0': ['\n# ','\n'], # headings
+        '1': ['\n## ','\n'],
+        '2': ['\n### ','\n'],
+        '3': ['\n#### ','\n'],
+        '4': ['\n##### ','\n'],
+        '5': ['\n###### ','\n'],
+        '6': ['**','** '], # bold
+        '7': ['*','* '], # italic
+        '8': ['***','*** '], # bold italic
+        '9': ['\n> ','\n'], # block quote
+        '10': ['\n>> ','\n'], # nested block quote
+        '11': ['\n>> # ','\n'],
+        '12': ['\n>> - ','\n'],
+        '13': ['\n>> 1. ','\n'],
+        '14': ['\n- ','\n'],
+        '15': ['`','` '],
+        '16': ['```\n','\n```'],
+        '17': ['\n>> `','`\n'],
+        '18': ['\n69. ','\n'],
+        '19': ['\n420. ','\n'],
+    }
+    for a in text.split():
+        r = m[str(random.randrange(len(m)))]
+        o += "%s%s%s" % (r[0], a, r[1])
+    return o
 
 def authenticate(url, token):
     return Mastodon(access_token=token, api_base_url=url)
-
-# database stuff
 
 def createDB(path = os.path.join(os.path.dirname(__file__), DB_PATH)):
     db = connectDB()
@@ -115,8 +154,6 @@ def dbCheck():
         createDB()
         log('DB created','info')
 
-# notifications
-
 def getCurrentNotificationsIndex(db):
     return getTimelineIndex(db, "notifications")
 
@@ -134,17 +171,17 @@ def processDM(noti):
 
 def processFavourite(noti):
     log("%s favorited" % (noti['account']['acct']))
-    print("\\\n%s\n/" % (noti['status']['content']))
+    print("\\\n%s\n/" % (stripHTML(noti['status']['content'])))
 
 def processReblog(noti):
     log("%s boosted" % (noti['account']['acct']))
-    print("\\\n%s\n/" % (noti['status']['content']))
+    print("\\\n%s\n/" % (stripHTML(noti['status']['content'])))
 
 def processMention(noti):
     log("%s mentioned" % (noti['account']['acct']))
     #if (getPostRepliedTo(noti['status'])):
     #    print("" % s (), )
-    print("\\\n%s\n/" % (noti['status']['content']))
+    print("\\\n%s\n/" % (stripHTML(noti['status']['content'])))
 
 def processFollow(noti):
     log("%s followed" % (noti['account']['acct']))
@@ -185,8 +222,6 @@ def processNotificationStreaming(notification):
         log(notification['type'], "error")
         log(json.dumps(notification, indent=4, sort_keys=False, default=str), "error")
 
-# posts
-
 def uploadImages(m, image_path_array):
     media_list = list()
     for i in image_path_array:
@@ -199,7 +234,17 @@ def uploadImages(m, image_path_array):
     return media_list
 
 def sendPost(m, status, in_reply_to_id=None, media_ids=None, sensitive=False, visibility=None, spoiler_text=None, language=None, idempotency_key=None, content_type=None, scheduled_at=None, poll=None):
-    return m.status_post(status, in_reply_to_id=in_reply_to_id, media_ids=media_ids, sensitive=sensitive, visibility=visibility, spoiler_text=spoiler_text, language=language, idempotency_key=idempotency_key, content_type=content_type, scheduled_at=scheduled_at, poll=poll)
+    return m.status_post(   status,
+                            in_reply_to_id=in_reply_to_id, 
+                            media_ids=media_ids, # list of up to 4 media IDs or media dicts as returned by media_post()
+                            sensitive=sensitive, # True/False
+                            visibility=visibility, # 'direct' - mentioned, 'private' - followers, 'unlisted', 'public'
+                            spoiler_text=spoiler_text, 
+                            language=language, # ISO 639-2 language codes
+                            idempotency_key=idempotency_key, # unique key to prevent double post
+                            content_type=content_type, # 'text/markdown', 'text/html', 'text/plain'
+                            scheduled_at=scheduled_at, 
+                            poll=poll)# poll object as returned by make_poll()
 
 def getPost(m, post_id):
     return m.status(id)
@@ -246,8 +291,6 @@ def getThreadOP(m=None, post=None, thread=None):
         thread = unrollThread(m, post)
     return thread[0]['account']['acct']
 
-# download functions
-
 def httpDownload(url, local_save_path):
     if DOWNLOAD_METHOD == "direct":
         downloadUrllibDirect(url, local_save_path)
@@ -258,24 +301,82 @@ def downloadUrllibDirect(url, local_save_path):
     filename = urllib.request.urlopen(urllib.request.Request(url, method='HEAD')).info().get_filename()
     urllib.request.urlretrieve(url, local_save_path + filename)
 
-# youtube-dl
-
-class ytdl_logger(object):
+class ytdlLogger(object):
     def debug(self, msg):
-        log(msg, "debug")
+        if (YTDL_VERBOSE):
+            log(msg, "YTDL")
 
     def warning(self, msg):
-        log(msg, "debug")
+        if (YTDL_VERBOSE):
+            log(msg, "YTDL")
 
     def error(self, msg):
-        log(msg, "error")
+        if(YTDL_VERBOSE):
+            log(msg, "YTDL")
 
-def ytdl_hook(d):
-    print(d['status'])
+def ytdlHook(d):
     if d['status'] == 'finished':
-        print('download completed')
+        if(YTDL_VERBOSE):
+            log('Status: %s' % (d['status']),'YTDL')
+            log('filename: %s' % (d['filename']),'YTDL')
+            log('downloaded_bytes: %s' % (d['downloaded_bytes']),'YTDL')
+            log('elapsed: %s' % (d['elapsed']),'YTDL')
+    if d['status'] == 'downloading':
+        if(YTDL_VERBOSE):
+            log('Status: %s' % (d['status']),'YTDL')
+            log('filename: %s' % (d['filename']),'YTDL')
+            log('tmpfilename: %s' % (d['tmpfilename']),'YTDL')
+            log('downloaded_bytes: %s' % (d['downloaded_bytes']),'YTDL')
+            log('total_bytes: %s' % (d['total_bytes']),'YTDL')
+            log('total_bytes_estimate: %s' % (d['total_bytes_estimate']),'YTDL')
+            log('elapsed: %s' % (d['elapsed']),'YTDL')
+            log('eta: %s' % (d['eta']),'YTDL')
+            log('speed: %s' % (d['speed']),'YTDL')
+            log('fragment_index: %s' % (d['fragment_index']),'YTDL')
+            log('fragment_count: %s' % (d['fragment_count']),'YTDL')
+    if d['status'] == 'error':
+        log('oops, error in ytdl download:\n%s' % (d),'error')
+        # here we would re-queue
 
-def list_playlist_urls(playlist_url):
+
+video_opts = {
+    'outtmpl': DOWNLOAD_DIRECTORY + '%(title)s.%(ext)s',
+    'format': 'bestvideo+bestaudio',
+    'no_color': True,
+    'nooverwrites': True,
+    'restrictfilenames': True,
+    'call_home': False,
+    'writedescription': True,
+    'writeinfojson': True,
+    'ignoreerrors': True,
+    'continuedl': True,
+    'download_archive': "video_archive.txt",
+    'logger': ytdlLogger(),
+    'progress_hooks': [ytdlHook],
+}
+
+audio_opts = {
+    'outtmpl': DOWNLOAD_DIRECTORY + '%(title)s.%(ext)s',
+    'format': 'bestaudio/best',
+    'no_color': True,
+    'nooverwrites': True,
+    'restrictfilenames': True,
+    'call_home': False,
+    'writedescription': True,
+    'writeinfojson': True,
+    'ignoreerrors': True,
+    'continuedl': True,
+    'download_archive': "audio_archive.txt",
+    'postprocessors': [{
+        'key': 'FFmpegExtractAudio',
+        'preferredcodec': 'mp3',
+        'preferredquality': '192',
+    }],
+    'logger': ytdlLogger(),
+    'progress_hooks': [ytdlHook],
+}
+
+def ytdlGetPlaylistUrls(playlist_url):
     out = []
     with youtube_dl.YoutubeDL({'outtmpl': '%(id)s%(ext)s', 'quiet':True,}) as ydl:
         result = ydl.extract_info(playlist_url, download=False)
@@ -286,51 +387,9 @@ def list_playlist_urls(playlist_url):
                 out.append(v)
     return out
 
-def download_video(video_url):
-    video_ydl_opts = {
-        'format': 'bestvideo+bestaudio',
-        'no_color': True,
-        'nooverwrites': True,
-        'restrictfilenames': True,
-        'call_home': False,
-        'writedescription': True,
-        'writeinfojson': True,
-        'ignoreerrors': True,
-        'continuedl': True,
-        'download_archive': "video_archive.txt",
-        'logger': ytdl_logger(),
-        'progress_hooks': [ytdl_hook],
-    }
-    with youtube_dl.YoutubeDL(video_ydl_opts) as ydl:
-        ydl.download([video_url])
-
-def download_audio(audio_url):
-    audio_ydl_opts = {
-        'format': 'bestaudio/best',
-        'no_color': True,
-        'nooverwrites': True,
-        'restrictfilenames': True,
-        'call_home': False,
-        'writedescription': True,
-        'writeinfojson': True,
-        'ignoreerrors': True,
-        'continuedl': True,
-        'download_archive': "audio_archive.txt",
-        'postprocessors': [{
-            'key': 'FFmpegExtractAudio',
-            'preferredcodec': 'mp3',
-            'preferredquality': '192',
-        }],
-        'logger': ytdl_logger(),
-        'progress_hooks': [ytdl_hook],
-    }
-    with youtube_dl.YoutubeDL(audio_ydl_opts) as ydl:
-        ydl.download([audio_test_url])
-
-# websocket streaming
-
-def processAnnouncementReaction(reaction):
-    pass
+def ytdlDownload(url, opts=video_opts):
+    with youtube_dl.YoutubeDL(opts) as ydl:
+        ydl.download([url])
 
 class StreamListener(mastodon.StreamListener):
     def on_notification(self, notification):
@@ -342,20 +401,22 @@ class StreamListener(mastodon.StreamListener):
     def on_conversation(self, conversation):
         processDM(conversation)
 
+    def handle_heartbeat(self):
+        pass
+
+    def on_announcement_reaction(self, reaction):
+        pass
+
+    def on_status_update(self, blah):
+        pass
+
+    def on_update(self, update):
+        pass
+
     def on_unknown_event(self, name, event):
         log("not sure what this is", "error")
         log(name, "error")
         log(json.dumps(event, indent=4, sort_keys=False, default=str), "error")
-
-    def handle_heartbeat(self):
-        pass
-        #log('exchanged heartbeats with the server','debug')
-
-    def on_announcement_reaction(self, reaction):
-        processAnnouncementReaction(reaction)
-
-    def on_status_update(self, blah):
-        pass
 
 def startStreaming(m):
     listener = StreamListener()
@@ -365,8 +426,6 @@ def startStreamingAsync(m):
     listener = StreamListener()
     return m.stream_user(listener, run_async=True, reconnect_async=True, reconnect_async_wait_sec=5)
 
-# main loop
-
 def main():
     args = parseArguments()
     dbCheck()
@@ -375,15 +434,13 @@ def main():
     m = authenticate(args['url'], args['token'])
     stream_handle = startStreamingAsync(m)
     while stream_handle.is_alive():
-        log('checking stream handle is receiving','debug')
-        log('%s' % (stream_handle.is_receiving()),'debug')
-        log('checking last read notification index','debug')
+        #log('checking stream handle is receiving','debug')
+        #log('%s' % (stream_handle.is_receiving()),'debug')
         n = getNotificationsFromIndex(m, last_read)
         if len(n) > 0:
             last_read = n[0]['id']
             log('updating last read notification: %s' % (last_read),'debug')
             setTimelineIndex(db, 'notifications', last_read)
-        log('sleeping main 5 minutes','debug')
         time.sleep(300)
     stream_handle.close()
 
